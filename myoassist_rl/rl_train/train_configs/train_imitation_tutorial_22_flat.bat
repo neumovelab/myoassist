9a2f31bf019db0056e30114c3918e3bdf0abd614
Hyoungseo Son <son.engr.kr@gmail.com>
2025-07-27 18:06:03 -0400

    Modified train .bat

--- myoassist_rl/rl_train/train_configs/train_imitation_tutorial_22_flat.bat
+++ myoassist_rl/rl_train/train_configs/train_imitation_tutorial_22_flat.bat
@@ -1,6 +1,2 @@
 python myoassist_rl/rl_train/train_ppo.py ^
---config_file_path myoassist_rl/rl_train/train_configs/imitation_tutorial_22_separated_net.json ^
---config.env_params.num_envs 1 --config.ppo_params.n_steps 256 ^
---config.ppo_params.batch_size 256 --config.logger_params.logging_frequency 1 ^
---config.logger_params.evaluate_frequency 2 --flag_rendering ^
 --config.env_params.terrain_type "flat"
