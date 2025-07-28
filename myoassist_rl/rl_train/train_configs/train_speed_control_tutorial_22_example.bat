843b8b4fcaf1a1189a362b86a2112f1dbb73d724
Hyoungseo Son <son.engr.kr@gmail.com>
2025-07-28 03:38:44 -0400

    Speed control transfer learning; Issue: evaluation wierd due to target speed maybe? - even the target speed changes during no speed control training

---
+++ myoassist_rl/rl_train/train_configs/train_speed_control_tutorial_22_example.bat
@@ -0,0 +1,3 @@
+python myoassist_rl/rl_train/train_ppo.py ^
+--config_file_path myoassist_rl/rl_train/train_configs/imitation_tutorial_22_separated_net_speed_control.json ^
+--config.env_params.prev_trained_policy_path docs/assets/tutorial_rl_models/train_session_20250727-230816_tutorial/session_name_models/session_name_step_9969664
