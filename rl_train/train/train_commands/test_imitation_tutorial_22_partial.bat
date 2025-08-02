36125e20c818abf017e3529d56e9a392369b9807
Hyoungseo Son <son.engr.kr@gmail.com>
2025-08-02 15:43:40 -0400

    renamed: run_policy.py -> run_train.py

--- rl_train/train/train_commands/test_imitation_tutorial_22_partial.bat
+++ rl_train/train/train_commands/test_imitation_tutorial_22_partial.bat
@@ -1,4 +1,4 @@
-python rl_train/run_policy.py ^
+python rl_train/run_train.py ^
 --config_file_path rl_train/train/train_configs/imitation_tutorial_22_separated_net_partial_obs.json ^
 --config.total_timesteps 12 ^
 --config.env_params.num_envs 1 --config.ppo_params.n_steps 4 ^
