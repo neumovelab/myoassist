7dec77b1ac61fda14e0d217807812647f2b8625f
Calder Robbins <robbins.cal@northeastern.edu>
2026-08-18 12:30:27 -0400

    add amp + stiffness configs

---
+++ ctrl_optim/optim/training_configs/kfoot_stiffness.bat
@@ -0,0 +1,21 @@
+@echo off
+python train.py ^
+    --msk myolegs22 ^
+    --device KFoot_L1 ^
+    --sim_time 20 ^
+    --pose_key walk_left ^
+    --num_strides 5 ^
+    --delayed 0 ^
+    --optim_mode single ^
+    --reflex_mode amp ^
+    --optimize_stiffness ^
+    --tgt_vel 1.25 ^
+    --trunk_err_type ref_diff ^
+    --tgt_sym_th 0.1 ^
+    --tgt_grf_th 1.5 ^
+    -kine ^
+    --popsize 32 ^
+    --maxiter 1000 ^
+    --threads 32 ^
+    --sigma_gain 10 ^
+    --save_path results/kfoot_stiffness
